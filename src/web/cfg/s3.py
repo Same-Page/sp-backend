37028f59ee9411b6f195b4cb61abfa264a2c3b0c
ArchiTech <swtdavid@gmail.com>
2020-05-30 01:22:59 -0700

    aws key

--- src/web/cfg/s3.py
+++ src/web/cfg/s3.py
@@ -1,2 +1,3 @@
 import os
+
 S3_BUCKET = os.environ.get('S3_BUCKET', 's3_bucket_missing')
--- src/web/clients/s3.py
+++ src/web/clients/s3.py
@@ -1,24 +1,25 @@
-import boto3, botocore
+import os
+
+import boto3
+import botocore
+
 from cfg.s3 import S3_BUCKET
 
-# TODO: restrict access to s3!
-# S3_KEY, S3_SECRET,
-# s3 = boto3.client("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
-s3 = boto3.client("s3")
+
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', 'AWS_ACCESS_KEY_missing')
+AWS_SECRET_ACCESS_KEY = os.environ.get(
+    'AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY_missing')
+
+s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 
 def upload_file(file, file_name, acl="public-read"):
 
-    try:
+    s3.upload_fileobj(
+        file,
+        S3_BUCKET,
+        file_name,
+        ExtraArgs={"ACL": acl, "ContentType": file.content_type},
+    )
 
-        s3.upload_fileobj(
-            file,
-            S3_BUCKET,
-            file_name,
-            ExtraArgs={"ACL": acl, "ContentType": file.content_type},
-        )
-
-    except Exception as e:
-        # This is a catch all exception, edit this part to fit your needs.
-        print("Something Happened: ", e)
-        return e
